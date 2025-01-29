9047442e52eea051dab85a56886696cb206437f7
aach1267 <aanchalsharmax1@gmail.com>
2025-01-29 23:40:42 +0530

    add file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        agustina: ['Great Vibes', 'sans-serif'], 
+      },
+      screens: {
+        'xs': '320px',
+      },
+      keyframes: {
+        neon: {
+          '0%': {
+            boxShadow: '0 0 5px #ff00ff, 0 0 10px #ff00ff, 0 0 15px #ff00ff',
+            borderColor: '#ff00ff',
+          },
+          '50%': {
+            boxShadow: '0 0 20px #00ffff, 0 0 30px #00ffff, 0 0 40px #00ffff',
+            borderColor: '#00ffff',
+          },
+          '100%': {
+            boxShadow: '0 0 5px #ff00ff, 0 0 10px #ff00ff, 0 0 15px #ff00ff',
+            borderColor: '#ff00ff',
+          },
+        },
+      },
+      animation: {
+        neon: 'neon 1.5s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
+
 }
 
