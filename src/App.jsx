94e19bb89c58dc032e1d63a5072559e17ae6d3fb
aach1267 <aanchalsharmax1@gmail.com>
2025-01-29 23:42:12 +0530

    Change some layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
+
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Outlet/>
     </div>
   )
 }
