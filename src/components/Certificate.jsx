9c98b8aa04d402e62864c40ebe0ead07122365bf
aach1267 <aanchalsharmax1@gmail.com>
2025-02-10 23:29:58 +0530

    Make responsive and little bit style related changes

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -4,24 +4,24 @@ import {learnings} from "../constants/index"
 
 function Certificate() {
   return (
-    <div className="flex justify-center items-center flex-col my-[120px]">
-      <div className="w-[900px]">
-        <img src={certificate} alt="certificate" />
+    <div className="flex justify-center items-center flex-col my-[120px] max-w-[1200px]  mx-auto px-10">
+      <div className="max-w-[800px] w-full">
+        <img src={certificate} alt="certificate" className="sm:w-[500px] xs:w-[600px] lg:w-[900px] rounded-[20px] mx-auto" />
       </div>
-      <div>
-        <div className="flex justify-between  items-center mx-auto text-[#16f2b3]  w-[900px] mt-6">
-          <h1 className="text-2xl font-black">What I learn....</h1>
-          <p>Nov2023 - Feb-2024</p>
+      <div className="w-full">
+        <div className="flex justify-between  items-center  text-[#16f2b3]  sm:max-w-[500px] md:max-w-[900px] mt-6 mx-auto">
+          <h1 className="lg:text-2xl  sm:text-xl font-black">What I learn....</h1>
+          <p className="md:text-xl text-[12px]">Nov2023 - April-2024</p>
         </div>
         {learnings.map((learning) => (
-            <div className="flex w-[900px] flex-col border-2 text-gray-200  border-[#1d293a] hover:border-[#464c6a] rounded-[20px] mt-5 p-4">
+            <div className="flex md:max-w-[900px] mx-auto w-full sm:max-w-[650px] flex-col flex-wrap  border-2 text-gray-200  border-[#1d293a] hover:border-[#464c6a] rounded-[20px] mt-5 p-4">
             <div className="flex mb-2 gap-2  xs:text-sm lg:text-xl text-xl items-center">
               <img
-                className="max-h-24 xs:max-h-4"
+                className="xs:hidden md:block md:max-h-[26px]"
                 src="./public/img/fast forward.png"
                 alt=".."
               />
-              <p className="text-white">
+              <p className="text-white xs:text-[13px] md:text-xl">
                 {learning}
               </p>
             
