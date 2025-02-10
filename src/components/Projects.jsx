6d0c5ff841617f781e114385c5ee910ed0290964
aach1267 <aanchalsharmax1@gmail.com>
2025-02-10 23:24:35 +0530

    Fix: Ensure navigation scrolls to top of page using window.scroll(0,0)

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,11 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import {Tilt} from "react-tilt";
 import { projects } from "../constants/index.js";
+import { Link } from "react-router-dom";
 
-// const Tags = styled.div`
-//   {
-//     width: 100%;
-//     display: flex;
-//     align-items: center;
-//     flex-wrap: wrap;
-//     gap: 8px;
-//     margin-top: 4px;
-//  }
+const handleClick=()=>{
+   window.scrollTo(0,0)
+}
 
 function Projects() {
   return (
@@ -20,13 +15,13 @@ function Projects() {
       whileInView={{ opacity: 1 }}
       viewport={{ once: true }}
       transition={{ duration: 1, delay: 0.5 }}
-      className="text-white font-black mt-[500px] lg:mt-[200px]"
+      className="text-white font-black mt-[500px] lg:mt-[200px] xs:mt-[90px]"
     >
-      <p className="text-center md:text-[60px] sm:text-[10px] xs:text-[20px] lg:text-[40px] font-black">
+      <p className="text-center md:text-[60px] sm:text-[10px] xs:text-[25px] lg:text-[40px] font-black" id="project">
         Project
       </p>
-      <div className="flex flex-wrap justify-evenly rounded-lg gap-2 mt-8">
-        {projects.slice(0, 3).map((pro) => (
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-auto place-items-center gap-2 rounded-lg mt-8 xs:mt-3">
+        {projects.slice(0, 3).map((pro,index) => (
           <>
           <Tilt
             key={pro.id}
@@ -35,39 +30,34 @@ function Projects() {
               scale:1,
               speed:450
             }}
-            className="max-w-[360px]  border border-[#1d293a] hover:border-[#464c6a]   rounded-[20px"
+            className={`max-w-[370px] md:max-w-[320px] xs:max-w-[290px] border border-[#1d293a] hover:border-[#464c6a] rounded-[20px] ${index === 2 ? "lg:block hidden":""} ${index === 1 ? "hidden md:block":""}`}
           >
-            <div className="overflow-hidden h-[300px] object-cover w-auto  cursor-pointer">
+            <div className="overflow-hidden h-[350px]  xs:h-[250px] p-5  object-cover w-auto  cursor-pointer">
               <img
                 src={pro.image}
                 alt="..."
                 className="w-full h-full rounded-t-lg"
               />
             </div>
-            <div className="bg-[#1b203e]  p-4 h-[350px]">
-              <h1 className="text-blue-200">{pro.title}</h1>
-              <h2>{pro.date}</h2>
+            <div className="bg-[#1b203e]  p-4 md:h-[190px] xs:h-[150px] rounded-b-[20px] text-sm">
+              <h1 className="text-[#16f2b3] mb-2">{pro.title}</h1>
               <p>{pro.description}</p>
-              <div className="flex flex-wrap gap-2">
+              {/* <div className="flex flex-wrap gap-2">
               {pro.tags?.map((tag,index) =>(
                  <h1 key={index} className="bg-gray-200 p-2 text-black  rounded-lg">{tag}</h1>
               ))} 
-              </div>
+              </div> */}
               
             </div>
           </Tilt>
           </>
         ))}
       </div>
-      <motion.button
-         whileHover={{
-           scale: 1.05,
-           boxShadow: "0px 0px 8px rgba(0,0,0,0.3",
-         }}
-         className="z-10 mx-auto mt-8 cursor-pointer font-bold text-gray-200  md:w-auto p-5 border border-purple-400 rounded-xl text-xl"
-       >
-         view more
-       </motion.button>
+      <div className="w-full flex justify-center">
+      <div className="cursor-pointer mx-auto font-bold text-gray-200 mt-5 md:w-auto md:p-5 xs:p-3 border border-purple-400 rounded-xl md:text-xl xs:text-sm">
+        <Link to="/project/all" onClick={handleClick}>view more</Link>
+      </div>
+</div>
     </motion.div>
   );
 }
