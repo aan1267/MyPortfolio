faa9e9e098194eeabb8f08cccc141b67823c51eb
aach1267 <aanchalsharmax1@gmail.com>
2025-02-12 23:11:33 +0530

    new paths add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client"
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import './style.css'
-import App from './App.jsx'
+import App from "./App";
+import ProjectAll from "./components/ProjectAll";
+import Hero from "./components/Hero";
+import Certificate from "./components/Certificate";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path:"/",
+        element:<Hero />,
+      },
+      {
+        path: "/project/all",
+        element:<ProjectAll/>,
+      },
+      {
+        path: "/mycertificate",
+        element:<Certificate/>,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
