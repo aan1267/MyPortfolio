3339a3cf08d34f69cab80980e41613530260d4a5
aach1267 <aanchalsharmax1@gmail.com>
2025-02-12 23:10:36 +0530

    Fix: some changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,9 +14,9 @@ function Skills() {
     whileInView={{ opacity: 1 }}
     viewport={{ once: true }}
     transition={{ duration: 1, delay:0.5}}
-    className="text-center text-white font-black mt-[150px] lg:mt-[200px]"
+    className="text-center text-white font-black lg:mt-[200px]"
   >
-     <h1 className="md:text-[60px] sm:text-[10px] xs:text-[20px] lg:text-[40px] font-black">Skills</h1>
+     <h1 className="md:text-[60px] sm:text-[10px] mt-[150px] xs:text-[25px] lg:text-[40px] "  id="skills">Skills</h1>
      <Marquee
       gradient={false}
       speed={200}
@@ -27,7 +27,7 @@ function Skills() {
       direction="left"
     >
   {skills.map( (skill) => (
-     <div className="border-t border-b py-20 xs:py-8 px-6 mt-3  border-[#23213b] lg:max-w-full md:max-w-50   mb-10  flex justify-center">
+     <div className="border-t border-b py-20 xs:py-8 px-6 mt-3  border-[#23213b] lg:max-w-full md:max-w-50 xs:max-w-[900px]  mb-10  flex justify-center">
          <div className="flex rounded-lg mx-auto border-purple-500 hover:border-purple-900 transition-all duration-500 hover:scale-[1.15]  border-2 p-6 cursor-pointer">
           <img key={skill} className="w-12 sm:w-13 md:w-20 lg:w-16" src={`./public/skills/${skill}.png`}/>
          </div>
