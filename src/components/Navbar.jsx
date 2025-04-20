dfd51f6733ea3c5fd880a39f0840b40b5429fd62
aach1267 <aanchalsharmax1@gmail.com>
2025-04-20 23:43:03 +0530

    Add Light/Dark functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,165 +1,212 @@
 import React, { useState } from "react";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { motion } from "framer-motion";
-import { BsFillPersonLinesFill,BsLaptop,BsCalendar3} from "react-icons/bs";
-import { Link as RouterLink, useLocation } from "react-router-dom";
+import { BsFillPersonLinesFill, BsLaptop, BsCalendar3 } from "react-icons/bs";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { FaRegMoon, FaSun } from "react-icons/fa";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
-  const {pathname}=useLocation()
-  
-  const handleClick=()=>{
-    window.scrollTo(0,0)
- }
+  const { pathname } = useLocation();
+
+  const { theme,setTheme} = useContext(ThemeContext);
+
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
 
   const toggleNav = () => {
     setOpen(!open);
   };
 
   const closeNav = () => {
-    setOpen(false);
+    setOpen(!open);
+  };
+
+  const handleLightDarkMode = (e) => {
+    e.stopPropagation()
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   const MenuVariants = {
     open: {
       y: 0, // Slides down to the top of the screen
       opacity: 1,
-      transition: { type: 'spring', stiffness: 100, damping: 25 }
+      transition: { type: "spring", stiffness: 100, damping: 25 },
     },
     closed: {
-      y: '-100%', // Hides it off the screen from the top
+      y: "-100%", // Hides it off the screen from the top
       opacity: 0,
-      transition: { type: 'spring', stiffness: 100, damping: 25 }
-    }
+      transition: { type: "spring", stiffness: 100, damping: 25 },
+    },
   };
 
-  
   return (
-    <div className="fixed top-0 left-0  w-full  bg-opacity-70 backdrop-blur-md z-50">
-      <div className="max-w-[1300px] flex justify-between text-gray-200 text-xl items-center mx-auto px-6 h-20">
-        <RouterLink to="/" className="lg:text-3xl md:text-3xl sm:text-3xl text-xl cursor-pointer relative z-10">
-         <span className="">&lt; </span>
-          <span className="font-agustina lg:px-[10px]  md:px-[10px] px-0 font-bold">
-            Aanchal Sharma{" "}
+    <div
+      className={`fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md  z-50  ${
+        theme === "dark" ? "bg-[#190b1f]" : "bg-gray-500"
+      }`}
+    >
+      <div className={`max-w-[1300px] flex justify-between items-center mx-auto lg:px-[90px] text-xl  px-6  h-20 ${theme == "light" ? "text-zinc-900" :"text-gray-200"}`}>
+        <RouterLink
+          to="/"
+          className="md:text-2xl  sm:text-3xl text-xl cursor-pointer"
+        >
+          <span>&lt; </span>
+          <span className="font-agustina lg:px-[10px] md:px-[10px] px-0 font-bold">
+            Aanchal Sharma
           </span>
           <span className=""> /&gt; </span>
         </RouterLink>
-        {
-          pathname === "/" &&(
-            <ul className="hidden md:flex gap-12 z-10 cursor-pointer relative">
+       <div className="lg:ml-80">
+       {pathname === "/" &&(
+          <ul className={`hidden md:flex gap-12 z-10 cursor-pointer relative items-center justify-center ${theme == "light" ? "text-zinc-900" :"text-gray-200"}`}>
             <li>
-              <ScrollLink to="skills" smooth={true} offset={-70} duration={500} className="text-white  hover:text-pink-500">
+              <ScrollLink
+                to="skills"
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className={`${theme === "light" ? "hover:text-yellow-400" : "hover:text-pink-500"}`}
+
+              >
                 Skills
               </ScrollLink>
             </li>
             <li>
-              <ScrollLink to="project"  smooth={true} offset={-70} duration={500} className="text-white  hover:text-pink-500">
+              <ScrollLink
+                to="project"
+                smooth={true}
+                offset={-70}
+                duration={500}
+                className={`${theme === "light" ? "hover:text-orange-300" : "hover:text-pink-500"}`}>
                 Projects
               </ScrollLink>
             </li>
             <li>
-              <RouterLink onClick={handleClick} to="/mycertificate" smooth={true} offset={-70} duration={500} className="text-white  hover:text-pink-500">
+              <RouterLink
+                onClick={handleClick}
+                to="/mycertificate"
+                className={`${theme === "light" ? "hover:text-orange-300" : "hover:text-pink-500"}`}
+              >
                 Certificate
               </RouterLink>
             </li>
             <li>
-              <ScrollLink to="experience" smooth={true} offset={-60} duration={500} className="text-white hover:text-pink-500">
+              <ScrollLink
+                to="experience"
+              >
+                Experience
+              </ScrollLink>
+            </li>
+            <div onClick={handleLightDarkMode} className={`hover:rounded-full w-12 h-12 active:scale-95 flex items-center justify-center  ${theme == "light" ? "hover:bg-gray-200" : "hover:bg-zinc-400"}`}>
+              {theme === "light" ? (
+                <FaRegMoon size={27} />
+              ) : (
+                <FaSun size={27} />
+              )}
+            </div>
+          </ul>
+        )}
+       </div>
+      
+        {/* issue face theme  not work even after click resolve  by  add z index */}
+        {pathname === "/" && (
+            <div className="flex  items-center justify-between">
+              <div onClick={handleLightDarkMode} className="flex items-center justify-center md:hidden cursor-pointer  w-12 h-12 hover:rounded-full  hover:bg-gray-900 px-3 z-50">
+              {theme === "light" ?(
+                <FaRegMoon size={24} />
+              ) : (
+                <FaSun size={24} />
+              )}
+             </div>
+             <div
+                onClick={
+                    toggleNav}
+                  
+                className="flex items-center justify-center md:hidden   text-gray-200 cursor-pointer hover:rounded-full w-12 h-12 hover:bg-gray-900 px-3 z-50"
+              >
+              {open ? (
+                <AiOutlineClose size={30} />
+              ) : (
+                <AiOutlineMenu size={30} />
+              )}
+              </div>
+            
+          </div>
+        )}
+      </div>
+
+      {pathname === "/" && (
+        <motion.div
+          initial={false}
+          animate={open ? "open" : "closed"}
+          variants={MenuVariants}
+          className="absolute left-0 w-full bg-[#101825] bg-opacity-75 flex flex-col  gap-8 items-center"
+        >
+          <ul className="font-semibold  text-white text-xl space-y-8 p-[20px] text-center">
+            <li>
+              <ScrollLink
+                to="about"
+                onClick={closeNav}
+                smooth={true}
+                offset={-60}
+                duration={500}
+                className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
+              >
+                <BsFillPersonLinesFill />
+                About
+              </ScrollLink>
+            </li>
+            <li>
+              <RouterLink
+                to="/mycertificate"
+                onClick={() => {
+                  closeNav();
+                  handleClick();
+                }}
+                className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
+              >
+                <BsFillPersonLinesFill />
+                Certificate
+              </RouterLink>
+            </li>
+            <li>
+              <ScrollLink
+                to="project"
+                onClick={closeNav}
+                smooth={true}
+                offset={-60}
+                duration={500}
+                className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
+              >
+                <BsLaptop />
+                Project
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                to="experience"
+                onClick={closeNav}
+                smooth={true}
+                offset={50}
+                duration={500}
+                className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
+              >
+                <BsCalendar3 />
                 Experience
               </ScrollLink>
             </li>
           </ul>
-          )
-        }
-
-        {
-          pathname === "/" &&(
-            <div onClick={toggleNav} className="md:hidden z-50  text-gray-200 cursor-pointer">
-            {open ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
-          </div>
-          )}
-        
-      </div>
-
-      {
-        pathname === "/" &&(
-        <motion.div
-        initial={false}
-        animate={open ? "open" : "closed"}
-        variants={MenuVariants}
-        className="absolute left-0 top-16 w-screen  bg-[#101825] bg-opacity-75   flex flex-col  gap-8 items-center"
-      >
-        <ul className="font-semibold  text-white text-xl space-y-8 p-[20px] text-center">
-          <li>
-            <ScrollLink
-              to="about"
-              onClick={closeNav}
-              smooth={true}
-              offset={-60}
-              duration={500}
-              className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
-            >
-              <BsFillPersonLinesFill/>About
-            </ScrollLink>
-          </li>
-          <li>
-            <RouterLink
-              to="/mycertificate"
-              onClick={() => { closeNav(); handleClick(); }}
-              smooth={true}
-              offset={-60}
-              duration={500}
-              className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
-            >
-              <BsFillPersonLinesFill/>Certificate
-            </RouterLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="project"
-              onClick={closeNav}
-              smooth={true}
-              offset={-60}
-              duration={500}
-            className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
-            >
-              <BsLaptop/>Project
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              to="experience"
-              onClick={closeNav}
-              smooth={true}
-              offset={50}
-              duration={500}
-              className="text-white flex items-center gap-2 cursor-pointer hover:text-pink-500"
-            >
-              <BsCalendar3/>Experience
-            </ScrollLink>
-          </li>
-        </ul>
-         {/* <ul className="bg-black">
-          <li>
-            <Link to="skill" smooth={true} offset={50} duration={500}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="project" smooth={true} offset={50} duration={500}>
-              Project
-            </Link>
-          </li>
-          <li>
-            <Link to="experience" smooth={true} offset={50} duration={500}>
-              Experience
-            </Link>
-          </li>
-        </ul> */}
-      </motion.div>
+        </motion.div>
       )}
     </div>
   );
 }
 
 export default Navbar;
+
+
