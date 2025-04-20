868cad4c0d770110ca54c6f2068370ece7442273
aach1267 <aanchalsharmax1@gmail.com>
2025-04-20 23:51:27 +0530

    Fix:some changes due to create  pages folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,19 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client"
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import './style.css'
-import App from "./App";
-import ProjectAll from "./components/ProjectAll";
-import Hero from "./components/Hero";
-import Certificate from "./components/Certificate";
+import ProjectAll from "./pages/ProjectAll";
+import Certificate from "./pages/Certificate";
+import AppLayout from "./layout/AppLayout";
+import Landingpage from "./pages/Landingpage";
+
 
 const router = createBrowserRouter([
   {
-    element: <App />,
+    element: <AppLayout />,
     children: [
       {
         path:"/",
-        element:<Hero />,
+        element:<Landingpage />,
       },
       {
         path: "/project/all",
